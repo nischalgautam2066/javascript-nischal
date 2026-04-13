79c6456a899f6274ccb833e0c59185e1610459a4
nischalgautam2066 <nischalgtm2066@gmail.com>
2026-04-13 12:45:11 +0000

    basictypeconversion

---
+++ startlearning/typeconversion.js
@@ -0,0 +1,13 @@
+//type conversen
+/*let score = "hitesh"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// "33" = 33
+// "3abc" = NaN
+// true = 1 ; false = 0*/
+
+let istrue=1
+let val = Boolean(istrue)
+console.log(val)
